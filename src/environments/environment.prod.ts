1c14663b79904e9bc4c29a4f629964d146262ff3
xing liu <xing.liu@gmx.de>
2024-09-14 23:35:37 +0100

    yaml, add firebase to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
   production: true,
   artists: 'http://127.0.0.1:8000/artists',
-  records: 'http://127.0.0.1:8000/records'
+  records: 'http://127.0.0.1:8000/records',
+  firebase: {
+      apiKey: "AIzaSyCFgYmj1IHdSHZ3b9N2JCd2VUOrQ2h9drU",
+    authDomain: "record-c4171.firebaseapp.com",
+    databaseURL: "https://record-c4171-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "record-c4171",
+    storageBucket: "record-c4171.appspot.com",
+    messagingSenderId: "387458401546",
+    appId: "1:387458401546:web:0ce38cc653ca34062b66f3",
+    measurementId: "G-D3R6DQR6NK"
+    }
 };
